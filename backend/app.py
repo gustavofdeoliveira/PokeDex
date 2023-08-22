b485ba63639a9249d3d01c9fec2c6b9e8ffc0e01
Henrique Marlon <henriquemarloncs@gmail.com>
2023-08-22 15:58:51 -0300

    feat(db): :zap: creating flask

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client['mongodb']
-collection = db['images']
+@app.route('/pokemon', methods=['GET'])
+def get_pokemon():
+    # Conexão com o MongoDB
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['pokedex']
+    collection = db['pokemon']
 
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
-    name = request.form['name']
-    image = request.files['image'].read()
+    # Consulta todos os documentos na coleção
+    pokemon_list = list(collection.find())
 
-    image_data = {'name': name, 'image': Binary(image)}
-    collection.insert_one(image_data)
+    # Transforma o resultado em um formato JSON
+    result = [{'name': pokemon['name'], 'url': pokemon['url']} for pokemon in pokemon_list]
 
-    return 'Image uploaded'
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
